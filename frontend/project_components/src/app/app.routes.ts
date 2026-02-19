dfd9914df45f96fc1bb9654b0997a3b94bf06b41
Marcos Andre <marcosandre1309@gmail.com>
2026-02-19 15:53:03 -0300

    Frontend. Páginas de Login e Cadastro de usuário

--- frontend/project_components/src/app/app.routes.ts
+++ frontend/project_components/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import {Login} from './pages/login/login';
+import {UserRegister} from "./pages/user_register/user_register";
 
 export const routes: Routes = [
     {path: 'login', component: Login},
-    {path: '', redirectTo: 'login', pathMatch: 'full'}
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'register', component: UserRegister}
 ];
---
+++ frontend/project_components/src/app/pages/user_register/user_register.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user_register',
+  imports: [],
+  templateUrl: './user_register.html',
+  styleUrl: './user_register.css',
+})
+export class UserRegister {
+
+}
