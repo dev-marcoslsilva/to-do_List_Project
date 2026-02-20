b03bd96d9ddb627657648c184c0fad6caf1ba7d5
Marcos Andre <marcosandre1309@gmail.com>
2026-02-19 22:08:35 -0300

    Adicionados nova-tarefa e redefinição de senha

--- frontend/project_components/src/app/app.routes.ts
+++ frontend/project_components/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import {Login} from './pages/login/login';
 import {UserRegister} from "./pages/user_register/user_register";
+import { PasswordReset } from './pages/password-reset/password-reset';
+import {NewTask} from './pages/new-task/new-task';
 
 export const routes: Routes = [
     {path: 'login', component: Login},
     {path: '', redirectTo: 'login', pathMatch: 'full'},
-    {path: 'register', component: UserRegister}
+    {path: 'register', component: UserRegister},
+    {path: 'newPassword', component: PasswordReset},
+    {path: 'newTask', component: NewTask}
 ];
