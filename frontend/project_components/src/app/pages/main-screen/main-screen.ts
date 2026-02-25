78ef51c3fd008ee7c0afa37f268eb9288b3b30b5
Marcos Andre <marcosandre1309@gmail.com>
2026-02-25 10:30:28 -0300

    Criado a funcionalidade de pesquisa inteligente. Nao testado ainda

--- frontend/project_components/src/app/pages/main-screen/main-screen.ts
+++ frontend/project_components/src/app/pages/main-screen/main-screen.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../../services/task';
 import { CommonModule } from '@angular/common';
+import { FormControl } from '@angular/forms';
+import { distinctUntilChanged, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-main-screen',
@@ -8,29 +10,70 @@ import { CommonModule } from '@angular/common';
   templateUrl: './main-screen.html',
   styleUrl: './main-screen.css',
 })
-export class MainScreen implements OnInit{
+export class MainScreen implements OnInit {
   tasks: any[] = [];
+  specialTasks: any[] = [];
+  nextTask: any[] = [];
   name: String = localStorage.getItem('name') || 'seja bem vindo!';
   currentDate: Date = new Date();
 
-  constructor(private taskService: TaskService){}
+  searchControl = new FormControl('');
+  suggestons: any[] = [];
+
+  constructor(private taskService: TaskService) {}
 
   ngOnInit(): void {
     this.loadingTasks();
+    this.loadingSpecialTasks();
+    this.loadingDailyTasks();
+    this.searchConfig();
   }
 
-  loadingTasks(){
+  loadingTasks() {
     this.taskService.getTasks().subscribe({
       next: (data) => {
         this.tasks = data;
-        console.log("Tarefas carregadas com sucesso");
+        console.log('Tarefas carregadas com sucesso');
       },
-      error: (error) =>{
-        console.error("Erro ao carregar as tarefas", error);
-      }
+      error: (error) => {
+        console.error('Erro ao carregar as tarefas', error);
+      },
     });
-      
-    
   }
-  
+
+  loadingSpecialTasks() {
+    this.taskService.getSpecialTasks().subscribe({
+      next: (data) => {
+        this.specialTasks = data;
+        console.log('Tarefas especiais carregadas com sucesso');
+      },
+      error: (error) => {
+        console.error('Erro ao carregar as tarefas especiais', error);
+      },
+    });
+  }
+
+  loadingDailyTasks() {
+    this.taskService.getNextTask().subscribe({
+      next: (data) => {
+        this.nextTask = data;
+        console.log('Tarefas diárias carregadas com sucesso');
+      },
+      error: (error) => {
+        console.error('Erro ao carregar as tarefas diárias', error);
+      },
+    });
+  }
+
+  searchConfig() {
+    this.searchControl.valueChanges
+      .pipe(debounceTime(400), distinctUntilChanged())
+      .subscribe((value) => {
+        if (value && value.length >= 4) {
+          this.taskService.getSuggestons(value).subscribe((res) => (this.suggestons = res));
+        } else {
+          this.suggestons = [];
+        }
+      });
+  }
 }
--- frontend/project_components/src/app/services/task.ts
+++ frontend/project_components/src/app/services/task.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,5 +12,20 @@ export class TaskService {
 
   getTasks(){
     return this.http.get<any[]>(this.API);
+  }
+
+  getSpecialTasks(){
+    return this.http.get<any[]>(`${this.API}/special`);
+  }
+
+  getNextTask(){
+    return this.http.get<any[]>(`${this.API}/daily`);
+  }
+
+  getSuggestons(term: string): Observable<any[]>{
+    
+    const params = new HttpParams().set('name' , term);
+
+    return this.http.get<any[]>(`${this.API}/search`, { params });
   }
 }
